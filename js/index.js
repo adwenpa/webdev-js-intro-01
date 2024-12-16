3d2f14f6c155c1898277fdd24bb2003c4d817907
adwenpa <joojo.abr@gmail.com>
2024-12-16 16:42:08 -0500

    read the concept content and experimented inside index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+const TEN = 10;
+
+TEN = 5
+// index.js:7 Uncaught TypeError: Assignment to constant variable.
+//    at index.js:7:5
+
+
+
+let bestFriend = "Mike";
+
+bestFriend = "Mary";
+
+*/
